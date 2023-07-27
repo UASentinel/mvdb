c7580cc6f2b5fb9c49fd0b8d98b034612f48148f
Федоренко Володимир <volodymyr.fedorenko@nure.ua>
2023-07-27 13:08:17 +0300

    Added profile photo uploading

--- src/WebUI/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/WebUI/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using MvDb.Application.Common.Interfaces;
 using MvDb.Domain.Entities;
 
 namespace WebUI.Areas.Identity.Pages.Account;
@@ -23,26 +24,24 @@ public class RegisterModel : PageModel
     private readonly SignInManager<ApplicationUser> _signInManager;
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly ILogger<RegisterModel> _logger;
-    private readonly IEmailSender _emailSender;
+    private readonly IImageService _imageService;
 
     public RegisterModel(
         UserManager<ApplicationUser> userManager,
         SignInManager<ApplicationUser> signInManager,
         ILogger<RegisterModel> logger,
-        IEmailSender emailSender)
+        IImageService imageService)
     {
         _userManager = userManager;
         _signInManager = signInManager;
         _logger = logger;
-        _emailSender = emailSender;
+        _imageService = imageService;
     }
 
     [BindProperty]
     public InputModel Input { get; set; }
 
     public string ReturnUrl { get; set; }
-
-    public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
     public class InputModel
     {
@@ -65,12 +64,14 @@ public class RegisterModel : PageModel
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [DataType(DataType.Upload)]
+        public IFormFile ProfilePhoto { get; set; }
     }
 
     public async Task OnGetAsync(string returnUrl = null)
     {
         ReturnUrl = returnUrl;
-        ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
     }
 
     public async Task<IActionResult> OnPostAsync(string returnUrl = null)
@@ -78,11 +79,14 @@ public class RegisterModel : PageModel
         var userRole = new IdentityRole("User");
 
         returnUrl ??= Url.Content("~/");
-        ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        
         if (ModelState.IsValid)
         {
             var user = new ApplicationUser { UserName = Input.UserName, Email = Input.Email };
+            user.ProfilePhotoLink = await _imageService.UploadProfilePhoto(Input.ProfilePhoto, user.Id);
+            
             var result = await _userManager.CreateAsync(user, Input.Password);
+
             if (!string.IsNullOrWhiteSpace(userRole.Name))
             {
                 await _userManager.AddToRolesAsync(user, new[] { userRole.Name });
@@ -92,26 +96,8 @@ public class RegisterModel : PageModel
             {
                 _logger.LogInformation("User created a new account with password.");
 
-                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                var callbackUrl = Url.Page(
-                    "/Account/ConfirmEmail",
-                    pageHandler: null,
-                    values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                    protocol: Request.Scheme);
-
-                await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                {
-                    return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                }
-                else
-                {
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-                    return LocalRedirect(returnUrl);
-                }
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return LocalRedirect(returnUrl);
             }
             foreach (var error in result.Errors)
             {
--- src/WebUI/ConfigureServices.cs
+++ src/WebUI/ConfigureServices.cs
@@ -15,6 +15,7 @@ public static class ConfigureServices
         services.AddDatabaseDeveloperPageExceptionFilter();
 
         services.AddScoped<ICurrentUserService, CurrentUserService>();
+        services.AddScoped<IImageService, ImageService>();
 
         services.AddHttpContextAccessor();
 
