1f66030b07fc6ee6a4b7ae84d0d6ee4dbb8713fd
Федоренко Володимир <volodymyr.fedorenko@nure.ua>
2023-08-01 11:20:23 +0300

    Fixed minor issues

---
+++ src/Domain/Common/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace MvDb.Domain.Common;
+
+public static class Constants
+{
+    // File names
+    public const string ProfilePhotoFileName = "profile_photo.png";
+
+    // Folder paths
+    public const string UsersFolderPathLocal = "images\\users";
+    public const string UsersFolderPathHost = "images/users";
+}
--- src/WebUI/Services/ImageService.cs
+++ src/WebUI/Services/ImageService.cs
@@ -1,7 +1,7 @@
 ﻿using System.Security.Claims;
-
 using MvDb.Application.Common.Interfaces;
 using MvDb.Application.Common.Models;
+using MvDb.Domain.Common;
 
 namespace MvDb.WebUI.Services;
 
@@ -10,39 +10,40 @@ public class ImageService : IImageService
     private readonly IWebHostEnvironment _webHostEnvironment;
     private readonly IHttpContextAccessor _httpContextAccessor;
     private readonly string _webRootPath;
+    private readonly string _hostPath;
 
     public ImageService(IWebHostEnvironment webHostEnvironment, IHttpContextAccessor httpContextAccessor)
     {
         _webHostEnvironment = webHostEnvironment;
-        _httpContextAccessor = httpContextAccessor;
         _webRootPath = _webHostEnvironment.WebRootPath;
+
+        _httpContextAccessor = httpContextAccessor;
+        var request = _httpContextAccessor.HttpContext.Request;
+        _hostPath = $"{request.Scheme}://{request.Host}{request.PathBase}";
     }
 
     public async Task<string> UploadProfilePhoto(IFormFile imageFile, string userId)
     {
-        var directoryPath = _webRootPath + "\\images\\users\\" + userId;
+        var directoryPath = $"{_webRootPath}\\{Constants.UsersFolderPathLocal}\\{userId}";
 
         if (!Directory.Exists(directoryPath))
         {
             Directory.CreateDirectory(directoryPath);
         }
 
-        var imagePath = directoryPath + "\\profile_photo.png";
+        var imagePath = $"{directoryPath}\\{Constants.ProfilePhotoFileName}";
 
         if (File.Exists(imagePath))
         {
             File.Delete(imagePath);
         }
 
-        using (FileStream fs = System.IO.File.Create(imagePath))
+        using (FileStream fs = File.Create(imagePath))
         {
             await imageFile.CopyToAsync(fs);
         }
 
-        var request = _httpContextAccessor.HttpContext.Request;
-        var hostPath = $"{request.Scheme}://{request.Host}{request.PathBase}/";
-
-        var imageWebPath = hostPath + "images/users/" + userId + "/profile_photo.png";
+        var imageWebPath = $"{_hostPath}/{Constants.UsersFolderPathHost}/{userId}/{Constants.ProfilePhotoFileName}";
 
         return imageWebPath;
     }
